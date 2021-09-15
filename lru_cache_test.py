226d82fc18956b82698cc91e5eea6eed892924a0
Joyce Chidiadi <jcchidiadi@gmail.com>
2021-09-15 15:10:33 -0600

    Update lru_cache_test.py

--- lru_cache_test.py
+++ lru_cache_test.py
@@ -4,7 +4,7 @@ import unittest
 
 class TestProgram(unittest.TestCase):
     def test_case_one(self):
-        lru_cache = program.LRUCache(10)
+        lru_cache = lru_cache.LRUCache(10)
         lru_cache.put_key_value("first", 10)
         lru_cache.put_key_value("second", 11)
         lru_cache.put_key_value("third", 12)
