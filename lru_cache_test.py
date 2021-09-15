d50df39f8b6a8bc30deaf95a70326a440a44b5ce
Joyce Chidiadi <jcchidiadi@gmail.com>
2021-09-15 15:16:20 -0600

    Update lru_cache_test.py

--- lru_cache_test.py
+++ lru_cache_test.py
@@ -8,7 +8,7 @@ class TestProgram(unittest.TestCase):
         lru_cache.put_key_value("first", 10)
         lru_cache.put_key_value("second", 11)
         lru_cache.put_key_value("third", 12)
-        self.assertEqual(lru_cache.get_value_from_key("first"))
+        self.assertEqual(lru_cache.get_value_from_key("first"), 10)
                          
 if __name__ == '__main__':
         unittest.main()
